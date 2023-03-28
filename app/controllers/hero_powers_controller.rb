1b08d0f3cd29cbc9a1bb894709d7b8ccb7642ed5
Fred Murigi <freddiemurigi@gmail.com>
2023-03-28 17:39:06 +0300

    updated controllers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,70 +1,25 @@
 class HeroPowersController < ApplicationController
-  before_action :set_hero_power, only: %i[ show edit update destroy ]
+  def create 
+      hero = Hero.find_by(id: hero_power_params[:hero_id])
+      power = Power.find_by(id: hero_power_params[:power_id])
+      hero_power = HeroPower.create(hero_power_params)
+      hero_power.hero = hero
+      hero_power.power = power
 
-  # GET /hero_powers or /hero_powers.json
-  def index
-    @hero_powers = HeroPower.all
-  end
-
-  # GET /hero_powers/1 or /hero_powers/1.json
-  def show
-  end
-
-  # GET /hero_powers/new
-  def new
-    @hero_power = HeroPower.new
-  end
-
-  # GET /hero_powers/1/edit
-  def edit
-  end
-
-  # POST /hero_powers or /hero_powers.json
-  def create
-    @hero_power = HeroPower.new(hero_power_params)
-
-    respond_to do |format|
-      if @hero_power.save
-        format.html { redirect_to hero_power_url(@hero_power), notice: "Hero power was successfully created." }
-        format.json { render :show, status: :created, location: @hero_power }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @hero_power.errors, status: :unprocessable_entity }
+      if hero_power 
+          render json: hero.to_json(only: [:id, :name, :super_name], 
+          include: [powers: { except: [:created_at, :updated_at]}])
+      else 
+          render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
       end
     end
-  end
-
-  # PATCH/PUT /hero_powers/1 or /hero_powers/1.json
-  def update
-    respond_to do |format|
-      if @hero_power.update(hero_power_params)
-        format.html { redirect_to hero_power_url(@hero_power), notice: "Hero power was successfully updated." }
-        format.json { render :show, status: :ok, location: @hero_power }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @hero_power.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /hero_powers/1 or /hero_powers/1.json
-  def destroy
-    @hero_power.destroy
-
-    respond_to do |format|
-      format.html { redirect_to hero_powers_url, notice: "Hero power was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hero_power
-      @hero_power = HeroPower.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
+  
+    private
+  
     def hero_power_params
-      params.require(:hero_power).permit(:strength, :user_id)
+      params.require(:hero_power).permit(:strength, :power_id, :hero_id)
     end
+
+   
+  
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,70 +1,34 @@
 class PowersController < ApplicationController
-  before_action :set_power, only: %i[ show edit update destroy ]
-
-  # GET /powers or /powers.json
   def index
-    @powers = Power.all
-  end
-
-  # GET /powers/1 or /powers/1.json
-  def show
-  end
-
-  # GET /powers/new
-  def new
-    @power = Power.new
-  end
-
-  # GET /powers/1/edit
-  def edit
-  end
-
-  # POST /powers or /powers.json
-  def create
-    @power = Power.new(power_params)
-
-    respond_to do |format|
-      if @power.save
-        format.html { redirect_to power_url(@power), notice: "Power was successfully created." }
-        format.json { render :show, status: :created, location: @power }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @power.errors, status: :unprocessable_entity }
-      end
+      powers = Power.all
+      render json: powers
     end
+  
+    def show
+      power = Power.find_by(id: params[:id])
+      if power
+        render json: power
+      else
+        render json: { error: "Power not found" }, status: :not_found
+      end
   end
 
-  # PATCH/PUT /powers/1 or /powers/1.json
   def update
-    respond_to do |format|
-      if @power.update(power_params)
-        format.html { redirect_to power_url(@power), notice: "Power was successfully updated." }
-        format.json { render :show, status: :ok, location: @power }
+      power = Power.find_by(id: params[:id])
+      if power
+        if power.update(power_params)
+          render json: power
+        else
+          render json: { errors: power.errors.full_messages }, status: :unprocessable_entity
+        end
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @power.errors, status: :unprocessable_entity }
+        render json: { error: "Power not found" }, status: :not_found
       end
     end
-  end
-
-  # DELETE /powers/1 or /powers/1.json
-  def destroy
-    @power.destroy
-
-    respond_to do |format|
-      format.html { redirect_to powers_url, notice: "Power was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_power
-      @power = Power.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
+  
+    private
+  
     def power_params
-      params.require(:power).permit(:name, :description)
+      params.require(:power).permit(:description)
     end
 end
